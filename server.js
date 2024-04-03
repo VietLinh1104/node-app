eea5d9042669026a456dcc3f6088fcdc1e940e70
VietLinh1104 <linhtx2004@gmail.com>
2024-04-03 23:35:40 +0700

    hello

--- server.js
+++ server.js
@@ -3,37 +3,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const mysql = require('mysql2');
-
-// Tạo kết nối với cơ sở dữ liệu MySQL
-const connection = mysql.createConnection({
-    host: 'localhost', // Địa chỉ máy chủ MySQL
-    user: 'root',      // Tên người dùng MySQL
-    password: '',  // Mật khẩu của người dùng MySQL
-    database: 'user' // Tên cơ sở dữ liệu MySQL bạn muốn kết nối
-});
-
-// Kết nối với cơ sở dữ liệu
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL database: ' + err.stack);
-        return;
-    }
-    console.log('Connected to MySQL database as id ' + connection.threadId);
-});
-
-// Sau khi kết nối, bạn có thể thực hiện các truy vấn MySQL thông qua connection
-// Ví dụ:
-connection.query('SELECT * FROM my_table', (error, results, fields) => {
-    if (error) {
-        console.error('Error executing query: ' + error.stack);
-        return;
-    }
-    console.log('Query results: ', results);
-});
-
-// Đóng kết nối sau khi hoàn thành
-connection.end();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
